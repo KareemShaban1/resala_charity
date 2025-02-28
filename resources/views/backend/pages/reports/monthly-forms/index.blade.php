03dad043593cd9fac471f161eddb7576fbcd3c00
KareemShaban1 <karim17196@fci.bu.edu.eg>
2025-02-28 19:02:38 +0200

    new updated

--- resources/views/backend/pages/reports/monthly-forms/index.blade.php
+++ resources/views/backend/pages/reports/monthly-forms/index.blade.php
@@ -72,8 +72,9 @@
                                 @include('backend.pages.reports.monthly-forms.partials.donors_with_forms_table', ['donorsWithForms' => $donorsWithForms])
                                 <!-- Pagination Links -->
                                 <div class="d-flex justify-content-center mt-3">
-                                    {{ $donorsWithForms->links() }}
-                                </div>
+    {{ $donorsWithForms->links('pagination::bootstrap-4') }}
+</div>
+
                             </div>
 
                         </div>
